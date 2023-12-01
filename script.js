29fd6b58a42a43174e5c98bb3833b56fe8aaab98
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-11-30 18:24:55 -0800

    add up and down and keypress

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 sonicLocation = {
-    top: 1,
-    left: 2
+    top: 0,
+    left: 1
 };
 
-
+let sonicElm = document.querySelector ("#sonic-box");
+// DRY 
+// Dont Repeat Yourself
 function move(left, top){
-  let sonicElm = document.querySelector ("#sonic-box");
   sonicElm.style.left = left*100 + 'px';
   sonicElm.style.top = top*100 + 'px';
 }
@@ -21,6 +22,14 @@ function moveRight(){
     sonicLocation.left++; // add 1
     move(sonicLocation.left, sonicLocation.top);
 }
+function moveDown(){
+    sonicLocation.top++; // add 1
+    move(sonicLocation.left, sonicLocation.top);
+}
+function moveUp(){
+    sonicLocation.top--; // subtract 1
+    move(sonicLocation.left, sonicLocation.top);
+}
 
 
 
@@ -29,13 +38,31 @@ function moveRight(){
 
 
 
-
-
-
-
+document.addEventListener("keydown", function(event){
+    if(event.key == "ArrowLeft"){
+        moveLeft();
+    } else if (event.key == "ArrowDown"){
+        moveDown();
+    } else if (event.key == "ArrowUp"){
+        moveUp();
+    } else if (event.key == "ArrowRight"){
+        moveRight();
+    }
+});
+let sonicRing = document.querySelector("#ring-box");
 function setRandomRing(){
-    let sonicRing = document.querySelector("#ring-box");
     sonicRing.style.left = Math.random() * 450 + "px";
     sonicRing.style.top = Math.random() * 450 + "px";
+}
+let pointsShown = document.querySelector("#points");
+let isAlive, points;
+isAlive = true;
+points = 0;
+while (isAlive){
+    if(((sonicELm.left + 25)==sonicRing.left)&&((sonicElm.top+25)==sonicRing.top)){
+        points++;
+        setRandomRing();
+        pointsShown.inner
+    }
 }
 setRandomRing();
